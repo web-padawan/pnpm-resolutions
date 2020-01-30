4914a101e2502a515e90d5bd147b4b271f4a404b
web-padawan <iamkulykov@gmail.com>
2020-01-30 11:22:59 +0200

    Fetch versions by running pnpmfile.js

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This is a demo how to pin dependency versions when using `pnpm`.
 Fetch versions first:
 
 ```sh
-node ./fetch-versions.js
+node pnpmfile.js --fetch
 ```
 
 Then install as usual:
@@ -18,14 +18,14 @@ pnpm i --shamefully-hoist=true
 
 ## Usage
 
-The files in this repository do the following:
+The `pnpmfile.js` in this repository does two different things.
 
-1. `fetch-versions.js` (must be executed before `pnpm install`)
+1. When executed manually: `node pnpmfile.js --fetch`:
 
   - fetch data from `https://registry.npmjs.org/@vaadin/vaadin-shrinkwrap`
   - save data including versions information to `versions.json` file
 
-2. `pnpmfile.js` executed by `pnpm` itself during installation
+2. When executed by `pnpm` itself during installation:
 
   - get the shrinkwrap package version from the root `package.json` dependencies
   - check `versions` in `versions.json` and get the shrinkwrap that we need
--- pnpmfile.js
+++ pnpmfile.js
@@ -1,8 +1,20 @@
 // Put this file into your projects root, together with "fetch-versions.js"
 const fs = require('fs');
+const http = require('http');
+const { promisify } = require('util');
 
-const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
-const versions = JSON.parse(fs.readFileSync('./versions.json', 'utf-8')).versions;
+http.get[promisify.custom] = function getAsync(options) {
+  return new Promise((resolve, reject) => {
+    http
+      .get(options, response => {
+        response.end = new Promise(resolve => response.on('end', resolve));
+        resolve(response);
+      })
+      .on('error', reject);
+  });
+};
+
+const get = promisify(http.get);
 
 let vaadinDeps;
 
@@ -20,6 +32,9 @@ function getVaadinDeps() {
   if (vaadinDeps) {
     return vaadinDeps;
   }
+  const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+  const versions = JSON.parse(fs.readFileSync('./versions.json', 'utf-8')).versions;
+
   // index.json file contains info about all the versions
   const version = getExactVersion(
     packageJson.dependencies['@vaadin/vaadin-shrinkwrap']
@@ -57,3 +72,17 @@ function readPackage(pkg) {
 
   return pkg;
 }
+
+(async function main() {
+  const args = process.argv.slice(2);
+  if (args[0] === '--fetch') {
+    console.log('fetching versions from npm');
+    const response = await get(
+      'http://registry.npmjs.org/@vaadin/vaadin-shrinkwrap'
+    );
+    let body = '';
+    response.on('data', chunk => (body += chunk));
+    await response.end;
+    fs.writeFileSync('versions.json', body);
+  }
+})();
